5933547eae142ecb34aad4023dce6baceda546c3
mladensvarcika <mladensvarc123@gmail.com>
2023-03-07 11:39:40 +0100

    more code

--- LED_blink/main.c
+++ LED_blink/main.c
@@ -1,8 +1,17 @@
-# include <avr/io.h>
-# include <stdint.h>
+# include < avr / io .h >
+# include < util / delay .h >
+# include < stdint .h >
 int16_t main ()
 {
-while (1);
-
+int16_t high_time = 500;
+int16_t low_time = 500;
+DDRB |= 1 << 5; // PB5 je izlaz
+while (1)
+{
+PORTB |= 1 << 5; // LED ON
+_delay_ms ( high_time ) ; // Pauza 1 s
+PORTB &= ~(1 << 5) ; // LED OFF
+_delay_ms ( low_time ) ; // Pauza 1 s
+}
 return 0;
 }
